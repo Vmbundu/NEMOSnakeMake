6435bcaf95176a843d34c29b268c0c3d11a38545
Vanday Bundu <vbundu@login-3.deepthought2.umd.edu>
2022-03-09 12:33:22 -0500

    fixed shell portion of Snakefiles

--- formatter_v2.py
+++ formatter_v2.py
@@ -30,17 +30,27 @@ def rule_stam (elem, line, out):
         for ele in r:
             out.write("\t " + ele + ",\n")
         return shell_stam(elem+1, line, out)
+    elif elem <= len(line)-1:
+        return shell_stam(elem+1, line, out)
     else:
-        return elem+1
+        return elem
 
 def shell_stam (elem, line, out):
     r = re.search(r':', line[elem])
     if (r == None):
         out.write("shell: \n")
-    while r == None and elem <= len(line) - 1:
+    while r == None:
+            line[elem] = line[elem].strip()
             out.write("\'" + line[elem] + "\';\n")
-            r = re.search(r':', line[elem])
-            elem += 1
+            #r = re.search(r':', line[elem])
+            if elem < len(line) - 1:
+                elem += 1
+                r = re.search(r':', line[elem])
+            elif elem == len(line) - 1:
+                out.write("\'" + line[elem] + "\';\n")
+                break
+            else:
+                break
     return elem
 #    else:
 #        return elem
@@ -48,7 +58,7 @@ def shell_stam (elem, line, out):
 
 
 lines = []
-with open('Testfile.copy','r') as f:
+with open('Testfile','r') as f:
     lines = f.readlines()
 
 out = open('Snakefile', 'w')
